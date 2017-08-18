1720973d817b8074d875f898c2cfc132d49983fd
doubleA <aayush962@gmail.com>
2017-08-18 11:19:29 +0530

    remove tests

--- test/user_test.js
+++ test/user_test.js
@@ -1,13 +1,13 @@
-const assert = require('assert');
-const User = require('../models/user');
-
-describe('Create User', () => {
-  it('save a new user', (done) => {
-    const aayush = new User({ name: 'aayush'});
-    aayush.save()
-      .then(() => {
-        assert(!aayush.isNew);
-        done();
-      });
-  });
-});
+// const assert = require('assert');
+// const User = require('../models/user');
+//
+// describe('Create User', () => {
+//   it('save a new user', (done) => {
+//     const aayush = new User({ name: 'aayush'});
+//     aayush.save()
+//       .then(() => {
+//         assert(!aayush.isNew);
+//         done();
+//       });
+//   });
+// });
